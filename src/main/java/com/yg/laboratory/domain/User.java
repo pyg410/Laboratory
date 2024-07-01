94f8040ed7e04068403f9a85e130e3cef1246f05
Cookie <pon05114@naver.com>
2024-07-01 15:54:51 +0900

    feat: userDao
    UserDao
    DaoFactory
    User

--- src/main/java/com/yg/laboratory/domain/User.java
+++ src/main/java/com/yg/laboratory/domain/User.java
@@ -1,2 +1,36 @@
-package com.yg.laboratory.domain;public class User {
+package com.yg.laboratory.domain;
+
+public class User {
+    String id;
+    String name;
+    String password;
+    public User(String name, String password, String id){
+        this.name=name;
+        this.password=password;
+        this.id=id;
+    }
+
+    public User(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
