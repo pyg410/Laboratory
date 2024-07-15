fc308cd9e238925ed582a1ceb037fa513db60367
Cookie <pon05114@naver.com>
2024-07-15 18:22:49 +0900

    feat: change title design

--- src/main/java/com/yg/laboratory/controller/HomeController.java
+++ src/main/java/com/yg/laboratory/controller/HomeController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Controller
 public class HomeController {
-    private static final String BLOG_TITLE = "Code Laboratory";
+    private static final String BLOG_TITLE = "CODE LABORATORY";
 
     private static final String BLOG_DESCRIPTION = "이것저것 재밌는 코딩 실험을 하는 자유로운 공간입니다.";
 
@@ -22,5 +22,9 @@ public class HomeController {
         List<String> boards = Arrays.asList("뚝딱뚝딱", "만드는 중", "조금만", "기다려주세요");
         model.addAttribute("boards", boards);
         return "home";
+    }
+    @GetMapping("/home1")
+    public String home1(Model model){
+        return "home1";
     }
 }
