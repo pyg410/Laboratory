0b39291b6ae65fbfcee41f26c2824bac70aa04af
Cookie <pon05114@naver.com>
2024-07-23 23:35:47 +0900

    feat: add BoardService interface

--- src/main/java/com/yg/laboratory/service/BoardService.java
+++ src/main/java/com/yg/laboratory/service/BoardService.java
@@ -1,2 +1,10 @@
-package com.yg.laboratory.service;public interface BoardService {
+package com.yg.laboratory.service;
+
+import com.yg.laboratory.dao.BoardDto;
+
+import java.util.List;
+
+public interface BoardService {
+
+    List<BoardDto> getBoardList();
 }
