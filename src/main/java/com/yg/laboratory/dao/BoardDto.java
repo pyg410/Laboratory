4d2a32518a7bb0bd42c37976a95a86923491336e
Cookie <pon05114@naver.com>
2024-07-23 23:34:33 +0900

    feat: add BoardDto

--- src/main/java/com/yg/laboratory/dao/BoardDto.java
+++ src/main/java/com/yg/laboratory/dao/BoardDto.java
@@ -1,2 +1,12 @@
-package com.yg.laboratory.dao;public record BoardDto() {
+package com.yg.laboratory.dao;
+
+import lombok.Builder;
+
+public record BoardDto(
+        Long id,
+        String name,
+        String url
+) {
+    @Builder
+    public BoardDto{}
 }
