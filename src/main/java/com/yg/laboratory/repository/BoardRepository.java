52a2237f0b491b45db35eb1409d563b40f51c7a7
Cookie <pon05114@naver.com>
2024-07-23 23:34:53 +0900

    feat: add BoardRepository

--- src/main/java/com/yg/laboratory/repository/BoardRepository.java
+++ src/main/java/com/yg/laboratory/repository/BoardRepository.java
@@ -1,2 +1,9 @@
-package com.yg.laboratory.repository;public interface BoardRepository {
+package com.yg.laboratory.repository;
+
+import com.yg.laboratory.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
