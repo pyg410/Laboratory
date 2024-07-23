1235adb170e698caa55ce7d744b7c95d0eb857b0
Cookie <pon05114@naver.com>
2024-07-23 23:37:19 +0900

    feat: add initial Board Entity

--- src/main/java/com/yg/laboratory/domain/Board.java
+++ src/main/java/com/yg/laboratory/domain/Board.java
@@ -1,11 +1,21 @@
-package com.yg.laboratory.controller;
+package com.yg.laboratory.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class Board {
-    public int id;
-    public String name;
 
-    public Board(int id, String name){
-        this.id= id;
-        this.name = name;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
 }
