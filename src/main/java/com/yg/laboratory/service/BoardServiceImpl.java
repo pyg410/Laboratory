e85137502aeb9812a6d4e2eedcf2fef2ed68adb7
Cookie <pon05114@naver.com>
2024-07-23 23:36:03 +0900

    feat: add dummy BoardServiceImpl

--- src/main/java/com/yg/laboratory/service/BoardServiceImpl.java
+++ src/main/java/com/yg/laboratory/service/BoardServiceImpl.java
@@ -1,8 +1,15 @@
 package com.yg.laboratory.service;
 
+import com.yg.laboratory.dao.BoardDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class BoardService {
-    
+public class BoardServiceImpl implements BoardService{
+
+    public List<BoardDto>  getBoardList(){
+        return List.of(BoardDto.builder().id(1L).name("홈").url("").build(),BoardDto.builder().id(2L).name("게시판").url("boards").build());
+    }
+
 }
